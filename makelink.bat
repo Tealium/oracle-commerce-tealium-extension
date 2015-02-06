912f864586905e8dea60026e06fae4c69f328eec
Victor Gubin <victor_gubin@epam.com>
2015-02-06 14:01:42 +0200

    Add .gitignore and link scripts for development

---
+++ makelink.bat
@@ -0,0 +1,3 @@
+rem run as administrator 
+mklink /D %DYNAMO_ROOT%\TealiumATG d:\work\Telium\TealiumATG\ 
+pause
---
+++ makelink.sh
@@ -0,0 +1,2 @@
+# /bin/bash
+ln -s ~/development/projects/TealiumATG ${DYNAMO_ROOT}/TealiumATG
