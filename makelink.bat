fcd4ed14aeba3a65b192d96d198e61f686bcbab5
Victor Gubin <victor_gubin@epam.com>
2015-02-17 19:49:24 +0200

    Changing windows mklink.bat for patching CRS with custom J2EE

--- makelink.bat
+++ makelink.bat
@@ -1,3 +1,4 @@
 rem run as administrator 
-mklink /D %DYNAMO_ROOT%\Tealium d:\work\Telium\TealiumATG\SiteCore\target\Tealium 
+mklink /D %DYNAMO_ROOT%\Tealium d:\work\Telium\TealiumATG\SiteCore\target\Tealium
+mklink /D  %DYNAMO_ROOT%\CommerceReferenceStore\Store\Storefront\j2ee-apps d:\work\Telium\TealiumATG\j2ee-apps 
 pause
